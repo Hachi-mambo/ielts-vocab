24d6e2f91369665cf7fb8ac4ccad9a2249bc07f4
Hachi-mambo <nv42bcvg7h@privaterelay.appleid.com>
2026-09-17 09:09:14 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='ielts-v2';
-const ASSETS=['./','./index.html','./app.js','./style.css','./jszip.min.js','./seed_words.json','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))).then(()=>self.skipWaiting()));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
